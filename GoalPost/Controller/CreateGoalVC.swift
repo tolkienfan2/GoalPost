ba0ff96324cdfdb74bf73b27d8dab7b412319426
Minni K Ang <minni.ang@gmail.com>
2017-09-06 12:32:20 -0400

    UI complete

---
+++ GoalPost/Controller/CreateGoalVC.swift
@@ -0,0 +1,36 @@
+//
+//  CreateGoalVC.swift
+//  GoalPost
+//
+//  Created by Minni K Ang on 2017-09-06.
+//  Copyright © 2017 CreativeIce. All rights reserved.
+//
+
+import UIKit
+
+class CreateGoalVC: UIViewController {
+
+    @IBOutlet weak var goalTextView: UITextView!
+    @IBOutlet weak var shortTermBtn: UIButton!
+    @IBOutlet weak var longTermBtn: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func shortTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func longTermBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func nextBtnPressed(_ sender: Any) {
+    }
+    
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
