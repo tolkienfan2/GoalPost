65857925f1e33d0e4813bc46d43791eda0b11acc
Minni K Ang <minni.ang@gmail.com>
2017-09-06 08:08:47 -0400

    Initial Commit

---
+++ GoalPost/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GoalPost
+//
+//  Created by Minni K Ang on 2017-09-06.
+//  Copyright © 2017 CreativeIce. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
