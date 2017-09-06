5846ce6ae645a5c87f94879ab9d3039e80c8461d
Minni K Ang <minni.ang@gmail.com>
2017-09-06 08:49:13 -0400

    initial commit

--- GoalPost/Controller/GoalsVC.swift
+++ GoalPost/Controller/GoalsVC.swift
@@ -8,18 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class GoalsVC: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func addGoalBtnPressed(_ sender: Any) {
     }
-
-
+    
 }
 
